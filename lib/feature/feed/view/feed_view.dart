50161438adcf107539625e035ee5aa97c7b9d3a9
Doğukan <dogukan.selam12@gmail.com>
2024-03-05 21:23:16 +0300

    FeedView Created And Updated

---
+++ lib/feature/feed/view/feed_view.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/widgets/buttons/base_elevated_button.dart';
+class FeedView extends StatelessWidget {
+  const FeedView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Text(
+              'Welcome to \nSmartHome, Doğukan',
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.w600,
+                color: Colors.grey[600],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: BaseElevatedButton(
+                      onPressed: () {},
+                      text: 'Overview',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: BaseElevatedButton(
+                      onPressed: () {},
+                      textColor: Colors.black,
+                      text: 'Productity',
+                      backgroundColor: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        //  const DailyProgressWidget(progress: 76)
+        ],
+      ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Padding(
+        padding: EdgeInsets.only(left: 4),
+        child: Text(
+          'Home',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      centerTitle: false,
+      actions: [
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search, color: Colors.black),
+          ),
+        ),
+      ],
+    );
+  }
+}
